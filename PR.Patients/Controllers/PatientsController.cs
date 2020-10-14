d72f5d207e2236bfa47cd9fccc18da7cf1d1c576
root <admin@example.com>
2020-10-14 18:00:40 +0200

    Dodaj pliki projektów.

---
+++ PR.Patients/Controllers/PatientsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PR.Patients.Model;
+
+namespace PR.Patients.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        private readonly PrDataContext _context;
+
+        public PatientsController(PrDataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Patients.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Register(Patient p)
+        {
+            _context.Patients.Add(p);
+            _context.SaveChanges();
+
+            return Created(uri: "/api/Patients/", p);
+
+        }
+
+    }
+}
